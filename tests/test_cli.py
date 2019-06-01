bfd6ba25f8f7fe39a00fc59412efbf3c949c3a37
Malcolm Jones <bossjones@theblacktonystark.com>
2019-05-31 22:36:49 -0400

    wip: testing actually works

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,7 +5,7 @@ from contextlib import contextmanager
 import pyconfig
 from ultron8.cli import cli, get_flag, set_flag, set_fact_flags
 from ultron8.paths import Paths
-from .conftest import fixtures_path
+from conftest import fixtures_path
 
 paths = Paths()
 
