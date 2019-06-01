cd013a7a87257375199a35a1cc173328b21e31fa
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-01 16:37:42 -0400

    feature: ci

---
+++ .ci/dc-ci-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+_UID=$(id -u)
+_GID=$(id -g)
+
+PR_SHA=$(git rev-parse HEAD)
+REPO_NAME=bossjones/ultron8-ci
+IMAGE_TAG=${REPO_NAME}:${PR_SHA}
+
+TAG="${IMAGE_TAG}"
+
+docker-compose -f docker-compose.ci.yml build
