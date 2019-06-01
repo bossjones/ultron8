4aed9998d89b6b1fdf8c9c22860a4b74140556d8
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-01 16:50:32 -0400

    feature: ci

--- .ci/pytest_runner.sh
+++ .ci/pytest_runner.sh
@@ -8,8 +8,14 @@ eval $( fixuid )
 
 # UID/GID now match user/group, $HOME has been set to user's home directory
 
-gosu developer pyenv shell 3.6.8
+# gosu developer pyenv shell 3.6.8
 
-gosu developer pip install -e .
+# gosu developer pip install -e .
 
-gosu developer py.test --cov-config .coveragerc --verbose --cov-report term --cov-report xml --cov=ultron8 tests
+# gosu developer py.test --cov-config .coveragerc --verbose --cov-report term --cov-report xml --cov=ultron8 tests
+
+pyenv shell 3.6.8
+
+pip install -e .
+
+py.test --cov-config .coveragerc --verbose --cov-report term --cov-report xml --cov=ultron8 tests
