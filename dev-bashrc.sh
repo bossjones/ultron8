fc868ce142009328d25adfa8699ec61149d7eda9
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-01 14:40:41 -0400

    feature: ci

--- dev-bashrc.sh
+++ dev-bashrc.sh
@@ -7,3 +7,6 @@ echo 'export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
+echo 'export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"' >> /etc/profile.d/developer.sh
+echo 'eval "$(pyenv init -)"' >> /etc/profile.d/developer.sh
+echo 'eval "$(pyenv virtualenv-init -)"' >> /etc/profile.d/developer.sh
