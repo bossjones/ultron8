9fad18a420794b57e190c3a998f75b8828f52439
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-01 16:55:13 -0400

    feature: ci

--- .ci/pytest_runner.sh
+++ .ci/pytest_runner.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
-set -x
+# set -x
 
-# UID/GID map to unknown user/group, $HOME=/ (the default when no home directory is defined)
+echo " [run] UID/GID map to unknown user/group, \$HOME=/ (the default when no home directory is defined)"
+echo -e "\n\n"
 
 eval $( fixuid )
 
-# UID/GID now match user/group, $HOME has been set to user's home directory
-
-# gosu developer pyenv shell 3.6.8
-
-# gosu developer pip install -e .
-
-# gosu developer py.test --cov-config .coveragerc --verbose --cov-report term --cov-report xml --cov=ultron8 tests
-
-echo " [run] You are here"
-`pwd`
+echo " [run] UID/GID now match user/group, \$HOME has been set to user's home directory"
 echo -e "\n\n"
+
+# FIXME: Add a flag to enable gosu when needed
+# gosu developer pyenv shell 3.6.8
+# gosu developer pip install -e .
+# gosu developer py.test --cov-config .coveragerc --verbose --cov-report term --cov-report xml --cov=ultron8 tests
 
 pushd /home/developer/app
 
