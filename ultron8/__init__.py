394d518f6b6417a05cbe68e3f108fa0d3d75bb6b
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-01 20:09:35 -0400

    wip: metadata

--- ultron8/__init__.py
+++ ultron8/__init__.py
@@ -12,3 +12,73 @@ __author_email__ = "jarvis@theblacktonystark.com"
 __maintainer__ = "bossjones"
 __maintainer_email__ = "jarvis@theblacktonystark.com"
 __github_username__ = "bossjones"
+
+# ------------------------------------------------
+# TODO: Going to need to create something like the following! But wait, is this for libraries only or applications?
+# ------------------------------------------------
+# from .connection import RedisConnection, create_connection
+# from .commands import (
+#     Redis, create_redis,
+#     create_redis_pool,
+#     GeoPoint, GeoMember,
+# )
+# from .pool import ConnectionsPool, create_pool
+# from .pubsub import Channel
+# from .sentinel import RedisSentinel, create_sentinel
+# from .errors import (
+#     ConnectionClosedError,
+#     ConnectionForcedCloseError,
+#     MasterNotFoundError,
+#     MultiExecError,
+#     PipelineError,
+#     ProtocolError,
+#     ReadOnlyError,
+#     RedisError,
+#     ReplyError,
+#     MaxClientsError,
+#     AuthError,
+#     ChannelClosedError,
+#     WatchVariableError,
+#     PoolClosedError,
+#     SlaveNotFoundError,
+#     MasterReplyError,
+#     SlaveReplyError,
+# )
+
+
+# __version__ = '1.2.0'
+
+# __all__ = [
+#     # Factories
+#     'create_connection',
+#     'create_pool',
+#     'create_redis',
+#     'create_redis_pool',
+#     'create_sentinel',
+#     # Classes
+#     'RedisConnection',
+#     'ConnectionsPool',
+#     'Redis',
+#     'GeoPoint',
+#     'GeoMember',
+#     'Channel',
+#     'RedisSentinel',
+#     # Errors
+#     'RedisError',
+#     'ReplyError',
+#     'MaxClientsError',
+#     'AuthError',
+#     'ProtocolError',
+#     'PipelineError',
+#     'MultiExecError',
+#     'WatchVariableError',
+#     'ConnectionClosedError',
+#     'ConnectionForcedCloseError',
+#     'PoolClosedError',
+#     'ChannelClosedError',
+#     'MasterNotFoundError',
+#     'SlaveNotFoundError',
+#     'ReadOnlyError',
+#     'MasterReplyError',
+#     'SlaveReplyError',
+# ]
