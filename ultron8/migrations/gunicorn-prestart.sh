85b415b6f75578d7ff21ccd58a0b6538246e5379
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-04 17:45:18 -0400

    WOOT: web runs

--- ultron8/migrations/gunicorn-prestart.sh
+++ ultron8/migrations/gunicorn-prestart.sh
@@ -2,11 +2,16 @@
 
 echo "Running inside /app/prestart.sh, you could add migrations to this file, e.g.:"
 
-echo "
-#! /usr/bin/env bash
+# echo "
+# #! /usr/bin/env bash
+
+# # Let the DB start
+# sleep 10;
+# # Run migrations
+# alembic upgrade head
+# "
 
 # Let the DB start
 sleep 10;
 # Run migrations
 alembic upgrade head
-"
