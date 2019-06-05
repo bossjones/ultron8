913324ca58ff0a4246586f6eddb8b96dd6b083c7
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-04 21:49:06 -0400

    FEATURE: alembic

--- ultron8/api/settings.py
+++ ultron8/api/settings.py
@@ -10,19 +10,19 @@ log = logging.getLogger(__name__)
 config = Config(".env")
 
 # # Main Configs
-DEBUG = config("ULTRON_DEBUG", cast=bool, default=False)
-TESTING = config("ULTRON_TESTING", cast=bool, default=False)
-# SECRET_KEY = config('ULTRON_SECRET_KEY', cast=Secret)
-# ALLOWED_HOSTS = config('ULTRON_ALLOWED_HOSTS', cast=CommaSeparatedStrings)
+DEBUG = config("DEBUG", cast=bool, default=False)
+TESTING = config("TESTING", cast=bool, default=False)
+# SECRET_KEY = config('SECRET_KEY', cast=Secret)
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=CommaSeparatedStrings)
 
 # # Redis
-# REDIS_ENDPOINT = config('ULTRON_REDIS_ENDPOINT', default='127.0.0.1')
-# REDIS_PORT = config('ULTRON_REDIS_PORT', default=6379, cast=int)
-# REDIS_DB = config('ULTRON_REDIS_DB', default=0, cast=int)
-# REDIS_PASSWORD = config('ULTRON_REDIS_PASSWORD', default=None, cast=Secret)
+# REDIS_ENDPOINT = config('REDIS_ENDPOINT', default='127.0.0.1')
+# REDIS_PORT = config('REDIS_PORT', default=6379, cast=int)
+# REDIS_DB = config('REDIS_DB', default=0, cast=int)
+# REDIS_PASSWORD = config('REDIS_PASSWORD', default=None, cast=Secret)
 
 # DB
-DATABASE_URL = config("ULTRON_DATABASE_URL")
+DATABASE_URL = config("DATABASE_URL")
 
 # Testing
-TEST_DATABASE_URL = config("ULTRON_TEST_DATABASE_URL")
+TEST_DATABASE_URL = config("TEST_DATABASE_URL")
