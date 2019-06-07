ff13033333c3b019ef648c153bbf8a39283b197d
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-06 22:05:06 -0400

    wip: updated migrations

--- ultron8/api/db_models/user.py
+++ ultron8/api/db_models/user.py
@@ -5,6 +5,7 @@ from ultron8.api.db.u_sqlite.base_class import Base
 
 
 class User(Base):
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True, index=True)
     full_name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
