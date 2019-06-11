4cfbb66240bb7df4b91f8abd5103669945c827e5
Malcolm Jones <bossjones@theblacktonystark.com>
2019-06-10 21:42:26 -0400

    wip: travis fixes

--- .ci/dc-build-cache-base.sh
+++ .ci/dc-build-cache-base.sh
@@ -3,6 +3,10 @@
 set -e
 
 _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Load utility bash functions
+source $_DIR/utility.sh
+
 export CONTAINER_UID=$(id -u)
 export CONTAINER_GID=$(id -g)
 
@@ -12,11 +16,17 @@ IMAGE_TAG=${REPO_NAME}:${PR_SHA}
 
 TAG="${IMAGE_TAG}"
 
-docker pull $REPO_NAME:base || true
-docker pull $REPO_NAME:runtime-image || true
-docker-compose -f docker-compose.base.yml build
-docker push $REPO_NAME:base || true
+header " [run] pull docker cache from docker hub if available"
+time docker pull $REPO_NAME:base || true
+time docker pull $REPO_NAME:runtime-image || true
+header " [run] docker-compose build 'base'"
+time docker-compose -f docker-compose.base.yml build
+header " [run] docker-compose push '$REPO_NAME:base'"
+time docker push $REPO_NAME:base || true
 
-docker pull $REPO_NAME:runtime-image || true
-docker-compose -f docker-compose.ci.yml build
-docker push $REPO_NAME:runtime-image || true
+header " [run] docker-compose pull '$REPO_NAME:runtime-image'"
+time docker pull $REPO_NAME:runtime-image || true
+header " [run] docker-compose build '$REPO_NAME:runtime-image'"
+time docker-compose -f docker-compose.ci.yml build
+header " [run] docker-compose push '$REPO_NAME:runtime-image'"
+time docker push $REPO_NAME:runtime-image || true
