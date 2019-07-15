41fc10a490b8c9d72390e68f0bd66c5a29415723
Malcolm Jones <bossjones@theblacktonystark.com>
2019-07-14 21:54:00 -0400

    Feature: rule models db

--- ultron8/api/db_models/rule.py
+++ ultron8/api/db_models/rule.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import DateTime
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
+from sqlalchemy import JSON
 from sqlalchemy import String
 from sqlalchemy import Unicode
 from sqlalchemy import UnicodeText
@@ -18,59 +19,64 @@ from sqlalchemy.sql import func
 from ultron8.api.db.u_sqlite.base_class import Base
 from ultron8.api.db_models.utils import ProxiedDictMixin
 
+from sqlalchemy.orm import backref
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 
-class RuleTypeParameter(Base):
-    """A parameter about a rule type."""
+from ultron8.api.db.u_sqlite.base_class import Base
+from ultron8.api.db_models.ultronbase import UIDFieldMixin, ContentPackResourceMixin
+from ultron8.consts import ResourceType
 
-    __tablename__ = "rule_type_parameter"
-
-    rule_id = Column("rule_id", Integer, ForeignKey("rule_type.id"), primary_key=True)
-    key = Column("key", Unicode(64), primary_key=True)
-    value = Column("value", UnicodeText)
+from ultron8.api.models.system.common import ResourceReference
 
 
-class RuleType(ProxiedDictMixin, Base):
-    """A rule type."""
+class RuleTypeDB(Base):
+    """A specific type of rule."""
 
-    __tablename__ = "rule_type"
-    id = Column("id", Integer, primary_key=True, index=True)
-    name = Column("name", Unicode(100))
-    ref_key = Column("ref_key", String(255))
-    ref_value = Column("ref_value", String(255))
-    # parameters = dictonary of RuleTypeParameter, w/ key,val
-    parameters = relationship(
-        "RuleTypeParameter", collection_class=attribute_mapped_collection("key")
-    )
+    __tablename__ = "rule_types"
 
-    # -------------------------------------------------------------------------
-    # SOURCE: https://docs.sqlalchemy.org/en/13/orm/examples.html#module-examples.vertical
-    # SOURCE: https://docs.sqlalchemy.org/en/13/_modules/examples/vertical/dictlike.html
-    # Vertical Attribute Mapping
-    # Illustrates "vertical table" mappings.
+    id = Column(Integer, primary_key=True, index=True)
+    enabled = Column("enabled", Boolean)
+    parameters = Column("parameters", JSON)
 
-    # A "vertical table" refers to a technique where individual attributes of an object are stored as distinct rows in a table. The "vertical table" technique is used to persist objects which can have a varied set of attributes, at the expense of simple query control and brevity. It is commonly found in content/document management systems in order to represent user-created structures flexibly.
 
-    # Two variants on the approach are given. In the second, each row references a "datatype" which contains information about the type of information stored in the attribute, such as integer, string, or date.
-    # -------------------------------------------------------------------------
-    _proxied = association_proxy(
-        "parameters",
-        "value",
-        creator=lambda key, value: RuleTypeParameter(key=key, value=value),
-    )
+class RuleTypeSpecDB(Base):
+    """A specific type of rule spec."""
 
-    def __init__(self, name, *args, **values):
-        super(RuleType, self).__init__(*args, **values)
-        self.name = name
+    __tablename__ = "rule_type_spec"
+
+    id = Column(Integer, primary_key=True, index=True)
+    ref = Column("ref", String(255))
+    parameters = Column("parameters", JSON)
 
     def __repr__(self):
-        return "RuleType(%r)" % self.name
+        result = []
+        result.append("RuleTypeSpecDB@")
+        result.append(str(id(self)))
+        result.append('<ref="%s", ' % self.ref)
+        result.append('parameters="%s">' % self.parameters)
+        return "".join(result)
 
-    @classmethod
-    def with_characteristic(self, key, value):
-        return self.parameters.any(key=key, value=value)
+
+class ActionExecutionSpecDB(Base):
+    """A specific type of rule spec."""
+
+    __tablename__ = "action_execution_spec"
+
+    id = Column(Integer, primary_key=True, index=True)
+    ref = Column("ref", String(255))
+    parameters = Column("parameters", JSON)
+
+    def __repr__(self):
+        result = []
+        result.append("ActionExecutionSpecDB@")
+        result.append(str(id(self)))
+        result.append('<ref="%s", ' % self.ref)
+        result.append('parameters="%s">' % self.parameters)
+        return "".join(result)
 
 
-class Rules(ProxiedDictMixin, Base):
+class RuleDB(UIDFieldMixin, Base):
     """Db Schema for Rules table. Specifies the action to invoke on the occurrence of a Trigger. It
     also includes the transformation to perform to match the impedance
     between the payload of a TriggerInstance and input of a action.
@@ -80,35 +86,182 @@ class Rules(ProxiedDictMixin, Base):
         action: Action to execute when the rule is tripped.
         status: enabled or disabled. If disabled occurrence of the trigger
         does not lead to execution of a action and vice-versa.
+
+    EXAMPLE:
+
+    ---
+    name: "rule_name"                      # required
+    pack: "examples"                       # optional
+    description: "Rule description."       # optional
+    enabled: true                          # required
+
+    trigger:                               # required
+        type: "trigger_type_ref"
+
+    criteria:                              # optional
+        trigger.payload_parameter_name1:
+            type: "regex"
+            pattern : "^value$"
+        trigger.payload_parameter_name2:
+            type: "iequals"
+            pattern : "watchevent"
+
+    action:                                # required
+        ref: "action_ref"
+        parameters:                        # optional
+            foo: "bar"
+            baz: "{{ trigger.payload_parameter_1 }}"
     """
+
+    RESOURCE_TYPE = ResourceType.RULE
+    UID_FIELDS = ["packs_name", "name"]
 
     __tablename__ = "rules"
 
     id = Column("id", Integer, primary_key=True, index=True)
-    # trigger_type = Column(String(255))
     name = Column("name", String(255))
-    pack = Column("pack", String(255))
-    description = Column("description", String(255))
-    enabled = Column("enabled", Boolean)
-    trigger = Column("trigger", String(255))
-    action = Column("action", String(255))
     ref = Column("ref", String(255))
+    description = Column("description", String(255))
+    # trigger_type = Column(String(255))
+    trigger = Column("trigger", String(255))
+
+    # FIXME: This needs to be RuleTypeSpecDB
     type = Column("type", String(255))
-    # type = relationship(
-    #     "RuleType", collection_class=attribute_mapped_collection("ref_key")
-    # )
-    # _proxied = association_proxy(
-    #     "type",
-    #     "ref_value",
-    #     creator=lambda key, value: RuleType(key=key, value=value),
-    # )
-    criteria = Column("criteria", String(255))
-    context = Column("context", String(255))
+    enabled = Column("enabled", Boolean)
+    # FIXME: This needs to be ActionExecutionSpecDB
+    # EG. action = me.EmbeddedDocumentField(ActionExecutionSpecDB)
+    action = Column("action", String(255))
+
+    packs_id = Column("packs_id", Integer, ForeignKey("packs.id"), nullable=True)
+    packs_name = Column("packs_name", Integer, ForeignKey("packs.name"), nullable=True)
+    # FIX: sqlalchemy Error creating backref on relationship
+    # https://stackoverflow.com/questions/26693041/sqlalchemy-error-creating-backref-on-relationship
+    pack = relationship(
+        "Packs", backref=backref("pack_rules", uselist=False), foreign_keys=[packs_id]
+    )
+
+    criteria = Column("criteria", JSON)
+    context = Column("context", JSON)
     created_at = Column(DateTime(timezone=True), server_default=func.utcnow())
     updated_at = Column(DateTime(timezone=True), onupdate=func.utcnow())
 
+    def __init__(self, *args, **values):
+        super(RuleDB, self).__init__(*args, **values)
+        self.ref = self.get_reference().ref
+        self.uid = self.get_uid()
 
-# if "__main__" == __name__:
+    def get_reference(self):
+        """
+        Retrieve referene object for this model.
+
+        :rtype: :class:`ResourceReference`
+        """
+        if getattr(self, "ref", None):
+            ref = ResourceReference.from_string_reference(ref=self.ref)
+        else:
+            ref = ResourceReference(pack=self.pack, name=self.name)
+
+        return ref
+
+    def __repr__(self):
+        result = []
+        result.append("RuleDB@")
+        result.append(str(id(self)))
+        result.append('<ref="%s>", ' % self.ref)
+        return "".join(result)
+
+
+# class RuleTypeParameter(Base):
+#     """A parameter about a rule type."""
+
+#     __tablename__ = "rule_type_parameter"
+
+#     rule_id = Column("rule_id", Integer, ForeignKey("rule_type.id"), primary_key=True)
+#     key = Column("key", Unicode(64), primary_key=True)
+#     value = Column("value", UnicodeText)
+
+
+# class RuleType(ProxiedDictMixin, Base):
+#     """A rule type."""
+
+#     __tablename__ = "rule_type"
+#     id = Column("id", Integer, primary_key=True, index=True)
+#     name = Column("name", Unicode(100))
+#     ref_key = Column("ref_key", String(255))
+#     ref_value = Column("ref_value", String(255))
+#     # parameters = dictonary of RuleTypeParameter, w/ key,val
+#     parameters = relationship(
+#         "RuleTypeParameter", collection_class=attribute_mapped_collection("key")
+#     )
+
+#     # -------------------------------------------------------------------------
+#     # SOURCE: https://docs.sqlalchemy.org/en/13/orm/examples.html#module-examples.vertical
+#     # SOURCE: https://docs.sqlalchemy.org/en/13/_modules/examples/vertical/dictlike.html
+#     # Vertical Attribute Mapping
+#     # Illustrates "vertical table" mappings.
+
+#     # A "vertical table" refers to a technique where individual attributes of an object are stored as distinct rows in a table. The "vertical table" technique is used to persist objects which can have a varied set of attributes, at the expense of simple query control and brevity. It is commonly found in content/document management systems in order to represent user-created structures flexibly.
+
+#     # Two variants on the approach are given. In the second, each row references a "datatype" which contains information about the type of information stored in the attribute, such as integer, string, or date.
+#     # -------------------------------------------------------------------------
+#     _proxied = association_proxy(
+#         "parameters",
+#         "value",
+#         creator=lambda key, value: RuleTypeParameter(key=key, value=value),
+#     )
+
+#     def __init__(self, name, *args, **values):
+#         super(RuleType, self).__init__(*args, **values)
+#         self.name = name
+
+#     def __repr__(self):
+#         return "RuleType(%r)" % self.name
+
+#     @classmethod
+#     def with_characteristic(self, key, value):
+#         return self.parameters.any(key=key, value=value)
+
+
+# class Rules(ProxiedDictMixin, Base):
+#     """Db Schema for Rules table. Specifies the action to invoke on the occurrence of a Trigger. It
+#     also includes the transformation to perform to match the impedance
+#     between the payload of a TriggerInstance and input of a action.
+#     Attribute:
+#         trigger: Trigger that trips this rule.
+#         criteria:
+#         action: Action to execute when the rule is tripped.
+#         status: enabled or disabled. If disabled occurrence of the trigger
+#         does not lead to execution of a action and vice-versa.
+#     """
+
+#     __tablename__ = "rules"
+
+#     id = Column("id", Integer, primary_key=True, index=True)
+#     # trigger_type = Column(String(255))
+#     name = Column("name", String(255))
+#     pack = Column("pack", String(255))
+#     description = Column("description", String(255))
+#     enabled = Column("enabled", Boolean)
+#     trigger = Column("trigger", String(255))
+#     action = Column("action", String(255))
+#     ref = Column("ref", String(255))
+#     type = Column("type", String(255))
+#     # type = relationship(
+#     #     "RuleType", collection_class=attribute_mapped_collection("ref_key")
+#     # )
+#     # _proxied = association_proxy(
+#     #     "type",
+#     #     "ref_value",
+#     #     creator=lambda key, value: RuleType(key=key, value=value),
+#     # )
+#     criteria = Column("criteria", String(255))
+#     context = Column("context", String(255))
+#     created_at = Column(DateTime(timezone=True), server_default=func.utcnow())
+#     updated_at = Column(DateTime(timezone=True), onupdate=func.utcnow())
+
+# smoke tests
+if "__main__" == __name__:
+    print("hi")
 #     rules = Rules()
 
 #     print(rules)
--- ultron8/api/db_models/trigger.py
+++ ultron8/api/db_models/trigger.py
@@ -8,12 +8,16 @@ from sqlalchemy import DateTime
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
 from sqlalchemy import String
+from sqlalchemy import JSON
+from sqlalchemy.orm import backref
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from ultron8.api.db.u_sqlite.base_class import Base
-from ultron8.api.db_models.ultronbase import UIDFieldMixin
+from ultron8.api.db_models.ultronbase import UIDFieldMixin, ContentPackResourceMixin
 from ultron8.consts import ResourceType
+
+from ultron8.api.models.system.common import ResourceReference
 
 
 class TriggerType(UIDFieldMixin, Base):
@@ -28,25 +32,49 @@ class TriggerType(UIDFieldMixin, Base):
     """
 
     RESOURCE_TYPE = ResourceType.TRIGGER_TYPE
-    UID_FIELDS = ["pack", "name"]
+    UID_FIELDS = ["packs_name", "name"]
 
     __tablename__ = "trigger_types"
 
     id = Column("id", Integer, primary_key=True, index=True)
+    name = Column("name", String(255))
     ref = Column("ref", String(255))
     uid = Column("uid", String(255), nullable=True)
-    name = Column("name", String(255))
     pack = relationship("Packs")
     packs_id = Column("packs_id", Integer, ForeignKey("packs.id"))
     description = Column("description", String(255))
-    payload_schema = Column("payload_schema", String(255))
-    parameters_schema = Column("parameters_schema", String(255))
+    payload_schema = Column("payload_schema", JSON)
+    parameters_schema = Column("parameters_schema", JSON)
+
+    packs_id = Column("packs_id", Integer, ForeignKey("packs.id"), nullable=True)
+    packs_name = Column("packs_name", Integer, ForeignKey("packs.name"), nullable=True)
+    # FIX: sqlalchemy Error creating backref on relationship
+    # https://stackoverflow.com/questions/26693041/sqlalchemy-error-creating-backref-on-relationship
+    pack = relationship(
+        "Packs", backref=backref("pack_actions", uselist=False), foreign_keys=[packs_id]
+    )
 
     def __init__(self, *args, **values):
         super(TriggerType, self).__init__(*args, **values)
         self.ref = self.get_reference().ref
         # pylint: disable=no-member
         self.uid = self.get_uid()
+
+    def get_reference(self):
+        """
+        Retrieve referene object for this model.
+
+        :rtype: :class:`ResourceReference`
+        """
+        if getattr(self, "ref", None):
+            ref = ResourceReference.from_string_reference(ref=self.ref)
+        else:
+            ref = ResourceReference(pack=self.pack, name=self.name)
+
+        return ref
+
+    def __repr__(self):
+        return "TriggerType<name=%s,ref=%s>" % (self.name, self.ref)
 
 
 class Trigger(UIDFieldMixin, Base):
@@ -59,23 +87,45 @@ class Trigger(UIDFieldMixin, Base):
     """
 
     RESOURCE_TYPE = ResourceType.TRIGGER
-    UID_FIELDS = ["pack", "name"]
+    UID_FIELDS = ["packs_name", "name"]
 
     __tablename__ = "triggers"
 
     id = Column("id", Integer, primary_key=True, index=True)
+    name = Column("name", String, nullable=False)
     ref = Column("ref", String(255))
     uid = Column("uid", String(255), nullable=True)
-    name = Column("name", String, nullable=False)
-    pack = relationship("Packs")
-    packs_id = Column("packs_id", Integer, ForeignKey("packs.id"))
     type = Column("type", String(255))
-    parameters = Column("parameters", String(255))
+    parameters = Column("parameters", JSON)
     ref_count = Column("ref_count", Integer)
 
+    packs_id = Column("packs_id", Integer, ForeignKey("packs.id"), nullable=True)
+    packs_name = Column("packs_name", Integer, ForeignKey("packs.name"), nullable=True)
+    # FIX: sqlalchemy Error creating backref on relationship
+    # https://stackoverflow.com/questions/26693041/sqlalchemy-error-creating-backref-on-relationship
+    pack = relationship(
+        "Packs",
+        backref=backref("pack_triggers", uselist=False),
+        foreign_keys=[packs_id],
+    )
+
     def __init__(self, *args, **values):
+        super(Trigger, self).__init__(*args, **values)
         self.ref = self.get_reference().ref
         self.uid = self.get_uid()
+
+    def get_reference(self):
+        """
+        Retrieve referene object for this model.
+
+        :rtype: :class:`ResourceReference`
+        """
+        if getattr(self, "ref", None):
+            ref = ResourceReference.from_string_reference(ref=self.ref)
+        else:
+            ref = ResourceReference(pack=self.pack, name=self.name)
+
+        return ref
 
     def get_uid(self):
         # Note: Trigger is uniquely identified using name + pack + parameters attributes
@@ -96,6 +146,9 @@ class Trigger(UIDFieldMixin, Base):
         # Note: We add 1 for parameters field which is not part of self.UID_FIELDS
         return len(parts) == len(self.UID_FIELDS) + 1 + 1
 
+    def __repr__(self):
+        return "Trigger<name=%s,ref=%s>" % (self.name, self.ref)
+
 
 class TriggerInstanceDB(Base):
     """An instance or occurrence of a type of Trigger.
@@ -109,10 +162,13 @@ class TriggerInstanceDB(Base):
 
     id = Column("id", Integer, primary_key=True, index=True)
     trigger = Column("trigger", String(255))
-    payload = Column("payload", String(255))
+    payload = Column("payload", JSON)
     occurrence_time = Column(DateTime(timezone=True), onupdate=func.utcnow())
 
     status = Column("status", String(255), nullable=False)
+
+    def __repr__(self):
+        return "TriggerInstanceDB<trigger=%s,payload=%s>" % (self.trigger, self.payload)
 
 
 MODELS = [TriggerType, Trigger, TriggerInstanceDB]
