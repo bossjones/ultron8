9eee2720be4007716393e86085117f6db3fe564d
Malcolm Jones <bossjones@theblacktonystark.com>
2019-09-08 02:00:36 -0400

    Feature: env vars

--- .ci/docker-test.sh
+++ .ci/docker-test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export TOXENV="py37"
+export PYTHON="python3"
+export DOCKER_COMPOSE_VERSION="1.24.0"
+export DOCKER_VERSION="18.09"
+export IS_CI_ENVIRONMENT="true"
+
 set -e
 
 _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
