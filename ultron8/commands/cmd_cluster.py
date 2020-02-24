2088cbff0485e551fa46375560f800cf2a62c664
Malcolm Jones <bossjones@theblacktonystark.com>
2020-02-24 02:54:28 -0500

    Test: working subcommand for 'cluster setup'

--- ultron8/commands/cmd_cluster.py
+++ ultron8/commands/cmd_cluster.py
@@ -14,15 +14,38 @@ logger = getLogger(__name__)
 
 stdin, stdout = sys.stdin, sys.stdout
 
+# ##############################################################################
+# # Catch exceptions and go into ipython/ipdb
+
+# from IPython.core.debugger import Tracer  # noqa
+# from IPython.core import ultratb
+
+# sys.excepthook = ultratb.FormattedTB(
+#     mode="Verbose", color_scheme="Linux", call_pdb=True, ostream=sys.__stdout__
+# )
+# ##############################################################################
+
 # for reference use: https://docs.d2iq.com/mesosphere/dcos/2.0/cli/
 
 
-@click.command("cluster", short_help="Manage your ultron8 clusters")
+@click.group("cluster", short_help="Manage your ultron8 clusters")
 @click.pass_context
 def cli(ctx):
-    """cluster cmd to setup workspace etc for ultron8."""
+    """cluster cmds for ultron8."""
     if get_flag("debug"):
         click.echo("Debug mode initiated")
         set_trace()
 
     logger.debug("cluster subcommand called from cli")
+
+
+@cli.command("setup")
+@click.argument("cluster_url", envvar="ULTRON_CLUSTER_URL")
+@click.pass_context
+def setup(ctx, cluster_url):
+    """Cmd to setup workspace etc for ultron8."""
+    if get_flag("debug"):
+        click.echo("Debug mode initiated")
+        set_trace()
+
+    logger.debug("cluster setup subcommand")
