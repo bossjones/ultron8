6aa9a12716700fe1590e95c3bc85fb10cd252a69
Malcolm Jones <bossjones@theblacktonystark.com>
2020-05-20 15:20:31 -0400

    WIP: endpoint_loggers

--- tests/api/api_v1/test_endpoint_loggers.py
+++ tests/api/api_v1/test_endpoint_loggers.py
@@ -51,12 +51,47 @@ class TestLoginApiEndpoint:
         assert logger.getEffectiveLevel() == 30
         assert r.json() == {"name": logger.name, "level": "warning"}
 
-        # url = app.url_path_for("logger_patch")
-        # response = client.patch(url, json={"name": logger.name, "level": "info"})
-        # assert response.status_code == 200
-        # logger.debug("should not be seen")
-        # assert logger.getEffectiveLevel() == 20
-        # assert response.json() == {"name": logger.name, "level": "info"}
+    # def test_read_root_change_loggers(client, caplog):
+    #     messages_expected = [
+    #         ("warn message", 30),
+    #         ("error message", 40),
+    #         ("critical message", 50),
+    #         ("critical message", 50),
+    #         ("debug message", 10),
+    #         ("info message", 20),
+    #         ("warn message", 30),
+    #         ("error message", 40),
+    #         ("critical message", 50),
+    #     ]
+    #     url_logger_get_main = app.url_path_for("logger_get", logger_name="main")
+    #     response = client.get(url_logger_get_main)
+    #     assert response.status_code == 200
+    #     assert response.json() == LoggerModel(name="main", level=30, children=[])
+
+    #     url_root = app.url_path_for("read_root")
+    #     # by default as main logger level is not set it's on 20
+    #     response = client.get(url_root)
+    #     assert response.status_code == 200
+    #     # setting it to critical
+    #     urlpatch = app.url_path_for("logger_patch")
+    #     response = client.patch(urlpatch, json={"name": "main", "level": "critical"})
+    #     assert response.status_code == 200
+    #     assert response.json() == {"name": "main", "level": "critical"}
+    #     # checking what we see in logs once it has been hit
+    #     response = client.get(url_root)
+    #     assert response.status_code == 200
+    #     # setting it to debug
+    #     urlpatch = app.url_path_for("logger_patch")
+    #     response = client.patch(urlpatch, json={"name": "main", "level": "debug"})
+    #     assert response.status_code == 200
+    #     assert response.json() == {"name": "main", "level": "debug"}
+    #     # checking what we see in logs once it has been hit
+    #     response = client.get(url_root)
+    #     assert response.status_code == 200
+
+    #     # so now we should have 1) warn-error-critical 2)-critical 3)-debug-info-warn-error-critical
+    #     for check in zip(messages_expected, caplog.records):
+    #         assert check[0][0] == check[1].message
 
 
 # ok @wshayes I got a loggers.py in my router that looks like that
