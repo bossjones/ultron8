156c053822ed52f6786bcb13b24241eb0846ea95
Malcolm Jones <bossjones@theblacktonystark.com>
2020-06-14 16:44:49 -0400

    feature: file structure documentation

--- ultron8/api/routers/action.py
+++
@@ -1,9 +0,0 @@
-"""Temporary router for Action objects. Taken directly from FastApi Tutorial"""
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("/v1/actions/", tags=["actions"])
-async def get_actions():
-    return [{"username": "Foo"}, {"username": "Bar"}]
--- ultron8/api/routers/alive.py
+++
@@ -1,6 +0,0 @@
-# from fastapi import APIRouter
-# from starlette.responses import UJSONResponse
-# router = APIRouter()
-# @router.get("/alive")
-# async def alive() -> UJSONResponse:
-#     return UJSONResponse({"yes": ":)"})
